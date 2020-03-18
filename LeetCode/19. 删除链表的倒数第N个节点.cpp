3575bfefc619897ff420b5ca90c6a45892281c33
Smartuil <563412673@qq.com>
2020-03-18 22:33:59 +0800

    19. 删除链表的倒数第N个节点

---
+++ LeetCode/19. 删除链表的倒数第N个节点.cpp
@@ -0,0 +1,27 @@
+#include "iostream"
+
+
+ struct ListNode {
+     int val;
+    ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+		ListNode *dummy = new ListNode(NULL);
+		dummy->next = head;
+		ListNode *first = dummy;
+		ListNode *second = dummy;
+		for (int i = 1; i <= n + 1; i++) {
+			first = first->next;
+		}
+		while (first != NULL) {
+			first = first->next;
+			second = second->next;
+		}
+		second->next = second->next->next;
+		return dummy->next;
+	}
+};
