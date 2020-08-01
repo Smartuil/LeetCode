1775bac0b19c8d3eea1f921f8620298cbf234974
Smartuil <563412673@qq.com>
2020-08-01 21:22:48 +0800

    🎇剑指 Offer 10- I. 斐波那契数列

---
+++ LeetCode/剑指 Offer 10- I. 斐波那契数列.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+
+using namespace std;
+
+class Solution {
+public:
+	int fib(int n) {
+		if (n == 0)return 0;
+		if (n == 1)return 1;
+		int a = 1, b = 0;
+		for (int i = 1; i < n; i++) {
+			a = a + b;
+			b = a - b;
+			a %= 1000000007;
+		}
+		return a;
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+	cout << solution->fib(100);
+	return 0;
+}
