5f5ed87bfdd1aa9e9bfc44bfee2fe277692be608
Smartuil <563412673@qq.com>
2020-08-02 22:26:25 +0800

    🎍剑指 Offer 14- II. 剪绳子 II

---
+++ LeetCode/剑指 Offer 14- II. 剪绳子 II.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+#include "vector"
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int cuttingRope(int n) {
+		if (n == 1 || n == 2) {
+			return 1;
+		}
+		if (n == 3) {
+			return 2;
+		}
+		long ret = 1;
+		while (n > 4) {
+			ret %= 1000000007;
+			ret = ret * 3 % 1000000007;
+			n -= 3;
+		}
+
+		if (n == 5) ret *= 2 * 3;
+		else if (n == 4) ret *= 2 * 2;
+		else if (n == 3) ret *= 3;
+		ret %= 1000000007;
+		return ret;
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+	cout << solution->cuttingRope(15);
+	return 0;
+}
