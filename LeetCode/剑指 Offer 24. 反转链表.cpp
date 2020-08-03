1e27f4f1d8dff3c55c16c5a26d50232b3e11e43d
Smartuil <563412673@qq.com>
2020-08-03 22:37:07 +0800

    🎟剑指 Offer 24. 反转链表

---
+++ LeetCode/剑指 Offer 24. 反转链表.cpp
@@ -0,0 +1,49 @@
+#include "iostream"
+#include "vector"
+#include <algorithm>
+#include <string>
+#include <map>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		ListNode* pre = nullptr;
+		ListNode* cur = head;
+
+		while (cur) {
+			ListNode* tmp = cur->next;
+			cur->next = pre;
+			pre = cur;
+			cur = tmp;
+		}
+		return pre;
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+
+	ListNode *a = new ListNode(1);
+	ListNode *b = new ListNode(2);
+	ListNode *c = new ListNode(3);
+	ListNode *d = new ListNode(4);
+	ListNode *e = new ListNode(5);
+	a->next = b;
+	b->next = c;
+	c->next = d;
+	d->next = e;
+	ListNode *ret = solution->reverseList(a);
+	while (ret) {
+		cout << ret->val << endl;
+		ret = ret->next;
+	}
+
+	return 0;
+}
