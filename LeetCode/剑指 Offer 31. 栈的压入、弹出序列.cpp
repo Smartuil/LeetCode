46649e1ee18e1bafc8916e8dd71d8f45ef655b9a
Smartuil <563412673@qq.com>
2020-08-04 22:51:32 +0800

    🎨剑指 Offer 31. 栈的压入、弹出序列

---
+++ LeetCode/剑指 Offer 31. 栈的压入、弹出序列.cpp
@@ -0,0 +1,49 @@
+#include "iostream"
+#include "string"
+#include "algorithm"
+#include "vector"
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+
+	bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+		stack<int> s;
+		int i = 0;
+		//while(i < pushed.size()) {
+		//	while(j < popped.size()) {
+		//		if (pushed[i] != popped[j]) {
+		//			s.push(pushed[i]);
+		//			i++;
+		//		}
+		//		else if(pushed[++i] == popped[++j])
+		//		{
+		//			j++;
+		//		}
+		//		else if(s.top() == popped[j])
+		//		{
+		//			j++;
+		//			s.pop();
+		//		}
+		//	}
+		//}
+		for (auto j : pushed) {
+			s.push(j);
+			while (!s.empty() && s.top() == popped[i]) {
+				s.pop();
+				i++;
+			}
+		}
+		return s.empty();
+	}
+};
+
+int main() {
+	Solution *solution = new Solution;
+	vector<int> pushed = { 1,2,3,4,5 };
+	vector<int> popped = { 4,5,3,2,1 };
+	cout << solution->validateStackSequences(pushed, popped);
+
+	return 0;
+}
