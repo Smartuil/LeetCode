c82a9f5c03af8ce5ce42500bc7e0027e5e66c664
Smartuil <563412673@qq.com>
2020-08-05 21:08:04 +0800

    🛒剑指 Offer 32 - I. 从上到下打印二叉树

---
+++ LeetCode/剑指 Offer 32 - I. 从上到下打印二叉树.cpp
@@ -0,0 +1,61 @@
+#include "iostream"
+#include "algorithm"
+#include "vector"
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	queue<TreeNode *> q;
+	void order(TreeNode *root, vector<int> &ret) {
+		TreeNode *tmp = root;
+		if (root) {
+			q.push(root);
+			//q.push(root->left->val);
+			//q.push(root->right->val);
+		}
+		while (!q.empty()) {
+			tmp = q.front();
+			if(tmp->left)
+				q.push(tmp->left);
+			if (tmp->right)
+				q.push(tmp->right);
+			ret.push_back(q.front()->val);
+			q.pop();		
+		}
+		
+	}
+
+	vector<int> levelOrder(TreeNode* root) {
+		vector<int> ret;
+		order(root, ret);
+		return ret;
+	}
+};
+
+int main() {
+	Solution *solution = new Solution;
+	TreeNode* root = new TreeNode(3, nullptr, nullptr);
+	TreeNode* a = new TreeNode(9, nullptr, nullptr);
+	TreeNode* b = new TreeNode(20, nullptr, nullptr);
+	TreeNode* c = new TreeNode(15, nullptr, nullptr);
+	TreeNode* d = new TreeNode(7, nullptr, nullptr);
+	root->left = a;
+	root->right = b;
+	b->left = c;
+	b->right = d;
+	vector<int> ret = solution->levelOrder(root);
+	for (auto i : ret) {
+		cout << i << endl;
+	}
+	return 0;
+}
---
+++ LeetCode/右边比他大的第一个数.cpp
@@ -0,0 +1,43 @@
+#include "iostream"
+#include "algorithm"
+#include "vector"
+#include <stack>
+using namespace std;
+
+vector<int> findMaxRight(vector<int> &arr) {
+	if(arr.size() == 0) {
+		return arr;
+	}
+
+	int arr_len = arr.size();
+	vector<int> ret(arr_len);
+	stack<int> s;
+	s.push(0);
+	int index = 1;
+	while (index< arr_len)
+	{
+		if (!s.empty() && arr[index] > arr[s.top()]) {
+			ret[s.top()] = arr[index];
+			s.pop();
+		}
+		else
+		{
+			s.push(index);
+			index++;
+		}
+	}
+	while (!s.empty()) {
+		ret[s.top()] = -1;
+		s.pop();
+	}
+	return ret;
+}
+
+int main() {
+	vector<int> a = { 8, 2, 5, 4, 3, 9, 7, 2, 5 };
+	vector<int> ret = findMaxRight(a);
+	for (auto i : ret) {
+		cout << i << " ";
+	}
+	return 0;
+}
