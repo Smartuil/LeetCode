2d32b1550eeab617f58ae4a559a6002be9941ab9
Smartuil <563412673@qq.com>
2020-08-08 12:03:44 +0800

    👛剑指 Offer 47. 礼物的最大价值

---
+++ LeetCode/剑指 Offer 47. 礼物的最大价值.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+#include "vector"
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	int maxValue(vector<vector<int>>& grid) {
+		int m = grid.size(), n = grid[0].size();
+		for (int j = 1; j < n; j++) // 初始化第一行
+			grid[0][j] += grid[0][j - 1];
+		for (int i = 1; i < m; i++) // 初始化第一列
+			grid[i][0] += grid[i - 1][0];
+		for (int i = 1; i < m; i++)
+			for (int j = 1; j < n; j++)
+				grid[i][j] += max(grid[i][j - 1], grid[i - 1][j]);
+		return grid[m - 1][n - 1];
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+	vector<vector<int>> grid = { {1,3,1},{1,5,1},{4,2,1} };
+	cout << solution->maxValue(grid);
+	return 0;
+}
