6b15093bd469bf218cd21a143a8f96758dc7ab16
Smartuil <563412673@qq.com>
2020-08-08 21:25:58 +0800

    👞剑指 Offer 53 - I. 在排序数组中查找数字 I

---
+++ LeetCode/剑指 Offer 53 - I. 在排序数组中查找数字 I.cpp
@@ -0,0 +1,57 @@
+#include "iostream"
+#include "vector"
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	int search(vector<int>& nums, int target) {
+	//	if (nums.size() == 0) {
+	//		return 0;
+	//	}
+	//	map<int, int> m;
+	//	int count = 0;
+	//	for (int i = 0; i < nums.size(); i++) {
+	//		m[nums[i]]++;
+	//	}
+	//	auto index = m.find(target);
+	//	if (index != m.end()) {
+	//		return index->second;
+	//	}
+	//	else
+	//	{
+	//		return 0;
+	//	}
+		
+		// 搜索右边界 right
+		int i = 0, j = nums.size() - 1;
+		while (i <= j) {
+			int m = (i + j) / 2;
+			if (nums[m] <= target) i = m + 1;
+			else j = m - 1;
+		}
+		int right = i;
+		// 若数组中无 target ，则提前返回
+		if (j >= 0 && nums[j] != target) return 0;
+		// 搜索左边界 right
+		i = 0; j = nums.size() - 1;
+		while (i <= j) {
+			int m = (i + j) / 2;
+			if (nums[m] < target) i = m + 1;
+			else j = m - 1;
+		}
+		int left = j;
+		return right - left - 1;
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+	vector<int> nums = { 5,7,7,8,8,10 };
+	cout << solution->search(nums, 8);
+	return 0;
+}
