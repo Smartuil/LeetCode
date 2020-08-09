ae738bd67b812ee6b38e0d93249f80315a3ccfa3
Smartuil <563412673@qq.com>
2020-08-09 15:37:38 +0800

    🎓剑指 Offer 58 - II. 左旋转字符串

---
+++ LeetCode/剑指 Offer 58 - II. 左旋转字符串.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+#include <sstream>
+#include "vector"
+#include <set>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	string reverseLeftWords(string s, int n) {
+		string tmp;
+		tmp = s.substr(0, n);
+		s.erase(0, n);
+		return s + tmp;
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+	cout << solution->reverseLeftWords("lrloseumgh", 6);
+	return 0;
+}
