a1e1e1c19db1b593d3e753311bde049cd051de31
Smartuil <563412673@qq.com>
2020-08-09 22:31:43 +0800

    💋剑指 Offer 59 - I. 滑动窗口的最大值

---
+++ LeetCode/剑指 Offer 59 - I. 滑动窗口的最大值.cpp
@@ -0,0 +1,71 @@
+#include "iostream"
+#include <sstream>
+#include "vector"
+#include <set>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <deque>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+		//if (nums.size() == 0) {
+		//	return { };
+		//}
+		//int i = 0;
+		//int j = i + k;
+		//int n;
+		//vector<int> tmp;
+		//vector<int> ret;
+		//while (j<=nums.size()) {
+		//	tmp.clear();
+		//	n = 0;
+		//	while(n<k){
+		//		tmp.push_back(nums[i+n]);
+		//		n++;
+		//	}
+
+		//	sort(tmp.begin(), tmp.end());
+		//	ret.push_back(tmp.back());
+		//	i++;
+		//	j = i + k;
+		//}
+		//return ret;
+
+		if (nums.size() == 0 || k == 0) {
+			return {};
+		}
+		deque<int> q;
+		vector<int> res(nums.size() - k + 1);
+		for (int i = 0; i < k; i++) {
+			while (!q.empty() && q.back() < nums[i]) {
+				q.pop_back();
+			}
+			q.push_back(nums[i]);
+		}
+		res[0] = q.front();
+		for (int i = k; i < nums.size(); i++) {
+			if (q.front() == nums[i - k]) {
+				q.pop_front();//滑动窗口后移，将第一个元素移除
+			}
+			while (!q.empty() && q.back() < nums[i]) {
+				q.pop_back();
+			}
+			q.push_back(nums[i]);
+			res[i - k + 1] = q.front();
+		}
+		return res;
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+	vector<int> nums = { 1,3,-1,-3,5,3,6,7 };
+	vector<int> ret = solution->maxSlidingWindow(nums, 3);
+	for (auto i : ret) {
+		cout << i << " ";
+	}
+	return 0;
+}
---
+++ LeetCode/剑指 Offer 60. n个骰子的点数.cpp
@@ -0,0 +1,25 @@
+#include "iostream"
+#include <sstream>
+#include "vector"
+#include <set>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <deque>
+using namespace std;
+
+class Solution {
+public:
+	vector<double> twoSum(int n) {
+
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+	vector<double> ret = solution->twoSum(1);
+	for (auto i : ret) {
+		cout << i << " ";
+	}
+	return 0;
+}
