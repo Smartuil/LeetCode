6787c1813abaf000b86b426c16958cd6b51018a1
Smartuil <563412673@qq.com>
2020-08-11 11:37:51 +0800

    💄剑指 Offer 60. n个骰子的点数

--- LeetCode/剑指 Offer 60. n个骰子的点数.cpp
+++ LeetCode/剑指 Offer 60. n个骰子的点数.cpp
@@ -11,13 +11,33 @@ using namespace std;
 class Solution {
 public:
 	vector<double> twoSum(int n) {
-
+		int dp[15][70];
+		memset(dp, 0, sizeof(dp));
+		for (int i = 1; i <= 6; i++) {
+			dp[1][i] = 1;
+		}
+		for (int i = 2; i <= n; i++) {
+			for (int j = i; j <= 6 * i; j++) {
+				for (int cur = 1; cur <= 6; cur++) {
+					if (j - cur <= 0) {
+						break;
+					}
+					dp[i][j] += dp[i - 1][j - cur];
+				}
+			}
+		}
+		int all = pow(6, n);
+		vector<double> ret;
+		for (int i = n; i <= 6 * n; i++) {
+			ret.push_back(dp[n][i] * 1.0 / all);
+		}
+		return ret;
 	}
 };
 
 int main() {
 	Solution *solution = new Solution();
-	vector<double> ret = solution->twoSum(1);
+	vector<double> ret = solution->twoSum(2);
 	for (auto i : ret) {
 		cout << i << " ";
 	}
