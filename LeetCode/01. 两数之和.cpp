825c068babfe53658c35bed133292c79e4dc3311
Smartuil <563412673@qq.com>
2020-08-13 15:40:23 +0800

    ⚽剑指 Offer 65. 不用加减乘除做加法

--- LeetCode/01. 两数之和.cpp
+++ LeetCode/01. 两数之和.cpp
@@ -1,26 +1,47 @@
 #include "iostream"
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
+
+//class Solution {
+//public:
+//	vector<int> twoSum(vector<int>& nums, int target) {
+//		for (int i = 0; i <= nums.size(); i++) {
+//			for (int j = 1; j <= nums.size() - 1; j++) {
+//				if ((nums[i] + nums[j]) == target || j > i) {
+//					return{ i,j };
+//				}
+//			}
+//		}
+//		return {1};
+//	}
+//	
+//};
 
 class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
-		for (int i = 0; i <= nums.size(); i++) {
-			for (int j = 1; j <= nums.size() - 1; j++) {
-				if ((nums[i] + nums[j]) == target || j > i) {
-					return{ i,j };
-				}
+		vector<int> ans;
+		unordered_map<int, int>hashmap;
+		for (int i = 0; i < nums.size(); i++) {
+			if (hashmap[target - nums[i]] && hashmap[target - nums[i]] != i + 1) {
+				//防止利用同个元素
+				ans.push_back(i);
+				ans.push_back(hashmap[target - nums[i]] - 1);
+				return ans;
 			}
+			hashmap[nums[i]] = i + 1;
+			//将hash表对应下标＋1，防止处理下标为0的情况
 		}
-		return {1};
+
+		return ans;
 	}
-	
 };
 
 int main() {
 	Solution *solution = new Solution();
-	vector<int> a = { 3,3 };
-	cout<<solution->twoSum(a, 6)[0]<< solution->twoSum(a, 6)[1];
+	vector<int> a = { 2, 7, 11, 15 };
+	cout<<solution->twoSum(a, 9)[0]<< solution->twoSum(a, 9)[1];
 	return 0;
 }
---
+++ LeetCode/剑指 Offer 65. 不用加减乘除做加法.cpp
@@ -0,0 +1,22 @@
+#include "iostream"
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+	int add(int a, int b) {
+		while (b != 0) { // 当进位为 0 时跳出
+			int c = (a & b) << 1;  // c = 进位
+			a ^= b; // a = 非进位和
+			b = c; // b = 进位
+		}
+		return a;
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+	cout << solution->add(-1, 2);
+	return 0;
+}
