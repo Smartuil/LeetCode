594ce2688cfc30036da802324d6c903180eb327f
Smartuil <563412673@qq.com>
2020-09-10 22:00:07 +0800

    🎧第一个错误的版本

---
+++ LeetCode/第一个错误的版本.cpp
@@ -0,0 +1,26 @@
+#include "iostream"
+#include "string"
+#include "vector"
+#include "algorithm"
+#include <queue>
+using namespace std;
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+	int firstBadVersion(int n) {
+		int i = 0;
+		while (i <= n) {
+			int mid = (i + n) / 2;
+			if()
+		}
+	}
+};
+
+int main() {
+	Solution *solution = new Solution();
+	cout << solution->firstBadVersion(5);
+	return 0;
+}
